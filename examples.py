599d029e5e3c293d2e95c0632c287b80b5270d6d
Xandra Campo <xkmpera@gmail.com>
2023-03-11 13:25:28 +0100

    Refactor examples

--- examples.py
+++ examples.py
@@ -6,6 +6,7 @@ from src.source.source import Source, Cf
 #  1. Create your own source modifying generic source attributes
 #  2. Create your own source as a new class
 #  3. Using example source
+#  4. Consistency check
 
 #  1. Create your own source modifying generic source attributes
 # ----------------------------------------------------------------------------------------------------------------------
@@ -79,97 +80,112 @@ print(fr)
 hr = cf.ambient_dose_equivalent_rate(date=date, distance=distance)
 print(hr)
 
+#  4. Consistency checks
 # ----------------------------------------------------------------------------------------------------------------------
 # Access to numeric attributes
 for attr, magnitude in cf.numeric_attributes():
     print(attr, magnitude)
 
-# ----------------------------------------------------------------------------------------------------------------------
-# Assigning attributes
-# Different ways to define or assign a negative value to an attribute:
-# Defining a Class(Source) with attribute with negative value
-# Assigning a Magnitude to attribute with negative value
-# Assigning a negative value to attribute.value
+# Check consistency: Different ways to define or assign a different value to an attribute:
 
+# 4.1. Defining a Class(Source):
 # ----------------------------------------------------------------------------------------------------------------------
-# Check consistency
-# Different ways to define or assign a different value to an attribute:
-# Defining a Class(Source):
+
+
 # with attribute with negative value
-# class MySource(Source):
-#     """252-Cf radionuclide neutron source from LPN."""
-#
-#     def __init__(self):
-#         super().__init__()
-#         self.name = '252-Cf'
-#         self.calibration_date = '2012/05/20'
-#         self.calibration_strength = Magnitude(value=-5.471E+08, unit='1/s', relative_uncertainty=0.013)
-#         self.half_life = Magnitude(value=2.6470, unit='y', uncertainty=0.0026)
-#         self.anisotropy_factor = Magnitude(value=1.051, unit='ND', uncertainty=0.019)
-#         self.linear_attenuation_coefficient = Magnitude(value=1055e-7, unit='1/cm', relative_uncertainty=0.015)
-#         self.fluence_to_dose_conversion_factor = Magnitude(value=385, unit='pSv·cm²', relative_uncertainty=0.01)
-#         self.neutron_effectiveness = Magnitude(value=0.5, unit='ND', uncertainty=0.1)
-#         self.total_air_scatter_component = Magnitude(value=0.00012, unit='1/cm', relative_uncertainty=0.15)
-#
-#
-# my_source = MySource()
+try:
+    class MySource(Source):
+        """252-Cf radionuclide neutron source from LPN."""
+
+        def __init__(self):
+            super().__init__()
+            self.name = '252-Cf'
+            self.calibration_date = '2012/05/20'
+            self.calibration_strength = Magnitude(value=-5.471E+08, unit='1/s', relative_uncertainty=0.013)
+            self.half_life = Magnitude(value=2.6470, unit='y', uncertainty=0.0026)
+            self.anisotropy_factor = Magnitude(value=1.051, unit='ND', uncertainty=0.019)
+            self.linear_attenuation_coefficient = Magnitude(value=1055e-7, unit='1/cm', relative_uncertainty=0.015)
+            self.fluence_to_dose_conversion_factor = Magnitude(value=385, unit='pSv·cm²', relative_uncertainty=0.01)
+            self.neutron_effectiveness = Magnitude(value=0.5, unit='ND', uncertainty=0.1)
+            self.total_air_scatter_component = Magnitude(value=0.00012, unit='1/cm', relative_uncertainty=0.15)
+
+
+    my_source = MySource()
+except ValueError as exc:
+    print(f'Raised ValueError: {exc}')
 # raises ValueError: Uncertainties must be positive.
 # expected ValueError: Source calibration strength must be positive.
 # TODO FAIL
 
 
 # with attribute with non-standard units
-# class MySource(Source):
-#     """252-Cf radionuclide neutron source from LPN."""
-#
-#     def __init__(self):
-#         super().__init__()
-#         self.name = '252-Cf'
-#         self.calibration_date = '2012/05/20'
-#         self.calibration_strength = Magnitude(value=5.471E+08, unit='s', relative_uncertainty=0.013)
-#         self.half_life = Magnitude(value=2.6470, unit='y', uncertainty=0.0026)
-#         self.anisotropy_factor = Magnitude(value=1.051, unit='ND', uncertainty=0.019)
-#         self.linear_attenuation_coefficient = Magnitude(value=1055e-7, unit='1/cm', relative_uncertainty=0.015)
-#         self.fluence_to_dose_conversion_factor = Magnitude(value=385, unit='pSv·cm²', relative_uncertainty=0.01)
-#         self.neutron_effectiveness = Magnitude(value=0.5, unit='ND', uncertainty=0.1)
-#         self.total_air_scatter_component = Magnitude(value=0.00012, unit='1/cm', relative_uncertainty=0.15)
-#
-#
-# my_source = MySource()
+try:
+    class MySource(Source):
+        """252-Cf radionuclide neutron source from LPN."""
+
+        def __init__(self):
+            super().__init__()
+            self.name = '252-Cf'
+            self.calibration_date = '2012/05/20'
+            self.calibration_strength = Magnitude(value=5.471E+08, unit='s', relative_uncertainty=0.013)
+            self.half_life = Magnitude(value=2.6470, unit='y', uncertainty=0.0026)
+            self.anisotropy_factor = Magnitude(value=1.051, unit='ND', uncertainty=0.019)
+            self.linear_attenuation_coefficient = Magnitude(value=1055e-7, unit='1/cm', relative_uncertainty=0.015)
+            self.fluence_to_dose_conversion_factor = Magnitude(value=385, unit='pSv·cm²', relative_uncertainty=0.01)
+            self.neutron_effectiveness = Magnitude(value=0.5, unit='ND', uncertainty=0.1)
+            self.total_air_scatter_component = Magnitude(value=0.00012, unit='1/cm', relative_uncertainty=0.15)
+
+
+    my_source = MySource()
+except ValueError as exc:
+    print(f'Raised ValueError: {exc}')
 # OK raises ValueError: Source calibration_strength units must be standard.
 
-# with negative uncertainty
 
-# class MySource(Source):
-#     """252-Cf radionuclide neutron source from LPN."""
-#
-#     def __init__(self):
-#         super().__init__()
-#         self.name = '252-Cf'
-#         self.calibration_date = '2012/05/20'
-#         self.calibration_strength = Magnitude(value=5.471E+08, unit='1/s', relative_uncertainty=-0.013)
-#         self.half_life = Magnitude(value=2.6470, unit='y', uncertainty=0.0026)
-#         self.anisotropy_factor = Magnitude(value=1.051, unit='ND', uncertainty=0.019)
-#         self.linear_attenuation_coefficient = Magnitude(value=1055e-7, unit='1/cm', relative_uncertainty=0.015)
-#         self.fluence_to_dose_conversion_factor = Magnitude(value=385, unit='pSv·cm²', relative_uncertainty=0.01)
-#         self.neutron_effectiveness = Magnitude(value=0.5, unit='ND', uncertainty=0.1)
-#         self.total_air_scatter_component = Magnitude(value=0.00012, unit='1/cm', relative_uncertainty=0.15)
-#
-#
-# my_source = MySource()
+# with negative uncertainty
+try:
+    class MySource(Source):
+        """252-Cf radionuclide neutron source from LPN."""
+
+        def __init__(self):
+            super().__init__()
+            self.name = '252-Cf'
+            self.calibration_date = '2012/05/20'
+            self.calibration_strength = Magnitude(value=5.471E+08, unit='1/s', relative_uncertainty=-0.013)
+            self.half_life = Magnitude(value=2.6470, unit='y', uncertainty=0.0026)
+            self.anisotropy_factor = Magnitude(value=1.051, unit='ND', uncertainty=0.019)
+            self.linear_attenuation_coefficient = Magnitude(value=1055e-7, unit='1/cm', relative_uncertainty=0.015)
+            self.fluence_to_dose_conversion_factor = Magnitude(value=385, unit='pSv·cm²', relative_uncertainty=0.01)
+            self.neutron_effectiveness = Magnitude(value=0.5, unit='ND', uncertainty=0.1)
+            self.total_air_scatter_component = Magnitude(value=0.00012, unit='1/cm', relative_uncertainty=0.15)
+
+
+    my_source = MySource()
+except ValueError as exc:
+    print(f'Raised ValueError: {exc}')
 # OK raise ValueError: Uncertainties must be positive.
 
-# Assigning a Magnitude to attribute
-cf = Cf()
-# cf.calibration_strength = Magnitude(value=-5.471E+08, unit='1/s', relative_uncertainty=0.013)
+# 4.2. Assigning a Magnitude to attribute
+# ----------------------------------------------------------------------------------------------------------------------
+try:
+    cf = Cf()
+    cf.calibration_strength = Magnitude(value=-5.471E+08, unit='1/s', relative_uncertainty=0.013)
+except ValueError as exc:
+    print(f'Raised ValueError: {exc}')
 # raises ValueError: Uncertainties must be positive.
 # expected ValueError: Source calibration strength must be positive.
 # TODO FAIL
-cf = Cf()
-# cf.calibration_strength = Magnitude(value=5.471E+08, unit='s', relative_uncertainty=0.013)
+try:
+    cf = Cf()
+    cf.calibration_strength = Magnitude(value=5.471E+08, unit='s', relative_uncertainty=0.013)
+except ValueError as exc:
+    print(f'Raised ValueError: {exc}')
 # OK raises ValueError: Source calibration_strength units must be standard.
-cf = Cf()
-# cf.calibration_strength = Magnitude(value=5.471E+08, unit='1/s', relative_uncertainty=-0.013)
+try:
+    cf = Cf()
+    cf.calibration_strength = Magnitude(value=5.471E+08, unit='1/s', relative_uncertainty=-0.013)
+except ValueError as exc:
+    print(f'Raised ValueError: {exc}')
 # OK raises ValueError: Uncertainties must be positive.
 cf = Cf()
 cf.calibration_strength = Magnitude(value=5.471E+07, unit='1/s', relative_uncertainty=0.013)
@@ -181,15 +197,30 @@ cf.calibration_strength = Magnitude(value=5.471E+08, unit='1/s', relative_uncert
 print(cf.calibration_strength)
 print(f'54710000.0 ± {5.471E+07 * 0.02} 1/s (2.0%)')
 
-# Assigning a negative value to attribute.value
-cf = Cf()
-cf.calibration_strength.value = -5.471E+08
+# 4.3. Assigning a negative value to attribute.value
+# ----------------------------------------------------------------------------------------------------------------------
+try:
+    pass
+except ValueError as exc:
+    print(f'Raised ValueError: {exc}')
+
+try:
+    cf = Cf()
+    cf.calibration_strength.value = -5.471E+08
+except ValueError as exc:
+    print(f'Raised ValueError: {exc}')
 # TODO FAIL: should raise Source calibration strength must be positive
-cf = Cf()
-cf.calibration_strength.units = 's'
+try:
+    cf = Cf()
+    cf.calibration_strength.units = 's'
+except ValueError as exc:
+    print(f'Raised ValueError: {exc}')
 # TODO FAIL: should raise non-standard unit error.
-cf = Cf()
-cf.calibration_strength.relative_uncertainty = -0.013
+try:
+    cf = Cf()
+    cf.calibration_strength.relative_uncertainty = -0.013
+except ValueError as exc:
+    print(f'Raised ValueError: {exc}')
 # TODO FAIL: should raise Source calibration strength must be positive
 cf = Cf()
 cf.calibration_strength.value = 5.471E+07
